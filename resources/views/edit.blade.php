d04bb909115128c759259e80cdeae35b26a09cde
IrsyadFHMI <fahmiirsyad77@gmail.com>
2023-12-04 10:05:29 +0700

    Tugas pertemuan tanggal 28 Nop

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -20,26 +20,42 @@
     <form action="/pegawai/update" method="post">
         {{ csrf_field() }}
         <input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
-        <div class="form-group">
-            <label for="nama">Nama</label>
-            <input type="text" class="form-control" required="required" name="nama" value="{{ $p->pegawai_nama }}">
+
+        <div class="form-group row">
+            <label for="nama" class="col-sm-1 col-control-label">Nama</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" required="required" name="nama" value="{{ $p->pegawai_nama }}">
+            </div>
         </div>
-        <div class="form-group">
-            <label for="jabatan">Jabatan</label>
-            <input type="text" class="form-control" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}">
+
+        <div class="form-group row">
+            <label for="jabatan" class="col-sm-1 col-control-label">Jabatan</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}">
+            </div>
         </div>
-        <div class="form-group">
-            <label for="umur">Umur</label>
-            <input type="number" class="form-control" required="required" name="umur" value="{{ $p->pegawai_umur }}">
+
+        <div class="form-group row">
+            <label for="umur" class="col-sm-1 col-control-label">Umur</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control" required="required" name="umur" value="{{ $p->pegawai_umur }}">
+            </div>
         </div>
-        <div class="form-group">
-            <label for="alamat">Alamat</label>
-            <textarea class="form-control" required="required" name="alamat">{{ $p->pegawai_alamat }}</textarea>
+
+        <div class="form-group row">
+            <label for="alamat" class="col-sm-1 col-control-label">Alamat</label>
+            <div class="col-sm-10">
+                <textarea class="form-control" required="required" name="alamat">{{ $p->pegawai_alamat }}</textarea>
+            </div>
         </div>
-        <input type="submit" class="btn btn-primary" value="Simpan Data">
+
+        <div class="form-group row">
+            <div class="col-sm-10 offset-sm-1">
+                <input type="submit" class="btn btn-primary" value="Simpan Data">
+            </div>
+        </div>
     </form>
     @endforeach
 </div>
-
 
 @endsection
